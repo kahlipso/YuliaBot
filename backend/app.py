024646039e461734d0b8b526ab56bf7ee41c6b91
kahlipso <ggl20172017@outlook.com>
2025-07-10 16:10:48 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,17 +4,16 @@ from backend.spotify_routes import spotify_bp
 from backend.twitch_routes import twitch_bp
 
 app = Flask(__name__)
+app.secret_key = "yulia_secret_keyy393884712"
+CORS(app)
 
 @app.route("/")
 def home():
     return redirect("http://www.yuliabot.net.s3-website-us-west-2.amazonaws.com")
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = "yulia_secret_keyy393884712"
-    CORS(app)
+app.register_blueprint(spotify_bp, url_prefix = "/spotify")
+app.register_blueprint(twitch_bp, url_prefix = "/twitch")
 
-    app.register_blueprint(spotify_bp, url_prefix = "/spotify")
-    app.register_blueprint(twitch_bp, url_prefix = "/twitch")
 
-    return app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
